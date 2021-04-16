5fedc0e683b14ffd704b6ac29fd5875e06d786cd
William Lachance <wrlach@gmail.com>
2021-04-16 14:46:11 -0400

    WIP

--- scripts/create-api.py
+++ scripts/create-api.py
@@ -2,6 +2,7 @@
 
 import json
 import os
+import sys
 from collections import defaultdict
 
 import frontmatter
@@ -9,36 +10,45 @@ import yaml
 
 ANNOTATIONS_DIR = os.path.join(os.path.dirname(__file__), "..", "annotations")
 
-data = defaultdict(lambda: defaultdict(lambda: {}))
+data = {}  # defaultdict(lambda: )
 
 apps = os.listdir(ANNOTATIONS_DIR)
 for app in apps:
     app_dir = os.path.join(ANNOTATIONS_DIR, app)
-    valid_features = []
+    valid_components = []
     try:
         metadata = yaml.load(open(os.path.join(app_dir, "metadata.yaml")))
-        valid_features = metadata.get('features', [])
+        data[app] = metadata
+        valid_components = metadata.get("components", [])
     except:
+        data[app] = {}
         pass
+    data[app]["annotations"] = defaultdict(lambda: {})
     for annotation_type in ("metrics", "pings"):
         annotation_dir = os.path.join(app_dir, annotation_type)
         if not os.path.isdir(annotation_dir):
             # for some apps, we may have annotations for one annotation type
             # but not another
             continue
-        annotation_ids = os.listdir(
-            annotation_dir
-        )
+        annotation_ids = os.listdir(annotation_dir)
         for annotation_id in annotation_ids:
-            annotation_md = frontmatter.load(
-                os.path.join(
-                    annotation_dir, annotation_id, "README.md"
-                )
+            annotation_filename = os.path.join(
+                annotation_dir, annotation_id, "README.md"
             )
+            annotation_md = frontmatter.load(annotation_filename)
             annotation = {"content": annotation_md.content}
-            for key in ["component", "features"]:
-                if annotation_md.get(key):
-                    annotation.update({key: annotation_md[key]})
-            data[app][annotation_type][annotation_id] = annotation
+            components = annotation_md.get("components")
+            if components:
+                invalid_components = [
+                    component not in valid_components for component in components
+                ]
+                if invalid_components:
+                    sys.stderr.write(
+                        f"Invalid components found in {annotation_filename}: {invalid_components}"
+                    )
+                    sys.exit(1)
+
+                annotation.update({"components": components})
+            data[app]["annotations"][annotation_type][annotation_id] = annotation
 
 print(json.dumps(data))
